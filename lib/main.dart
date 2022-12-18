bdb6dde8725a9d72a38463a30ec91dd97cfcdfe6
Ryle_Zamayla <joshuaryle4107@gmail.com>
2022-12-18 21:56:03 +0800

    Updated code and added a date formatter on project Dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:todo_assignment2/models/database_model.dart';
+import 'package:todo_assignment2/models/todos_model_LocalDB.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_assignment2/screens/todo_list.dart';
+import 'package:todo_assignment2/screens/user_input.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Setting up local Database using PHPmyAdmin'),
+      home: const MyHomePage(title: 'Setting up LocalDB using sqflite'),
     );
   }
 }
@@ -30,20 +35,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var db = DatabaseConnect();
+
+  void addItem(Todo todo) async {
+    await db.insertTodo(todo);
+    setState(() {});
+  }
+
+  void deleteItem(Todo todo) async {
+    await db.deleteTodo(todo);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            widget.title,
-            style: const TextStyle(
-                fontSize: 18
-            ),
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+              fontSize: 18
           ),
         ),
         leading: const Icon(Icons.dataset_linked),
+      ),
+      body: Column(
+        children: [
+          Todolist(insertFunction: addItem, deleteFunction: deleteItem),
+          // we will add our widgets here.
+          UserInput(insertFunction: addItem),
+        ],
       ),
     );
   }
